07574eaa6103b35a6d3c34b2f2778e7821cb6557
haha0103 <hinalinahili2008@gmail.com>
2025-05-08 14:54:48 +0900

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,67 @@
+package hello;
+
+public class MyClass {
+	public static void main(String[] args) {
+		/*byte byteValue= 65;
+		//char charValue= byteValue;
+		char charValue =(char)byteValue;//명시적 변환
+	  //음수X                  부호O 음수O
+		System.out.println(charValue);*/
+		
+		/*double myDouble = 9.78;
+		int myint = (int)myDouble;
+		//명시적 형 변환
+		System.out.println(myDouble);
+		System.out.println(myint);*/
+		
+		/*int intValue=10;
+		double doubleValue=5.5;0
+		double result = intValue+doubleValue;//자동 형 변환
+		System.out.println(result);*/
+		
+		/*//산술 연산자
+		int result = 10;
+		result = result/2;
+		System.out.println(result);
+		int a =3;
+		
+		System.out.println(1%a);
+		System.out.println(2%a);
+		System.out.println(3%a);
+		System.out.println(4%a);
+		a=10;
+		int b=3;
+		float c=10.0f;
+		float d=3.0f;
+		System.out.println(a/b);
+		System.out.println(c/d);
+		System.out.println(a/d);
+		
+		//단항 연산자
+		int i=3;
+		i++; //i = i+1
+		System.out.println(i);
+		System.out.println(i++);//i를 사용한 후 증가
+		System.out.println(i);
+		System.out.println(++i);//i를 증가한 후 사용
+		
+		//대입 연산자
+		int x=10;
+		System.out.println(x);
+		x=5;
+		x-=3;
+		System.out.println(x);
+		x=5;
+		x+=3;
+		System.out.println(x);
+		x=5;
+		x*=3;
+		System.out.println(x);
+		x=5;
+		x/=3;
+		System.out.println(x);
+		x=5;
+		x%=3;
+		System.out.println(x);*/
+	}
+}
