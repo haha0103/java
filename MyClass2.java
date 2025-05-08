bee0e1a39db41b3178cca9c540d638a04ca47886
haha0103 <hinalinahili2008@gmail.com>
2025-05-08 14:55:18 +0900

    Add files via upload

---
+++ MyClass2.java
@@ -0,0 +1,168 @@
+package help;
+
+import java.util.Scanner;//1)Scanner할려면 해야함
+
+public class MyClass2 {
+		public static void main(String[] args) {
+			Scanner axb = new Scanner(System.in);//1)
+			      //Scanner 이름
+			
+			/*byte byteValue= 65;
+			//char charValue= byteValue;
+			char charValue =(char)byteValue;//명시적 변환
+		  //음수X                  부호O 음수O
+			System.out.println(charValue);*/
+			
+			/*double myDouble = 9.78;
+			int myint = (int)myDouble;
+			//명시적 형 변환
+			System.out.println(myDouble);
+			System.out.println(myint);*/
+			
+			/*int intValue=10;
+			double doubleValue=5.5;0
+			double result = intValue+doubleValue;//자동 형 변환
+			System.out.println(result);*/
+			
+			/*//산술 연산자
+			int result = 10;
+			result = result/2;
+			System.out.println(result);
+			int a =3;
+			
+			System.out.println(1%a);
+			System.out.println(2%a);
+			System.out.println(3%a);
+			System.out.println(4%a);
+			a=10;
+			int b=3;
+			float c=10.0f;
+			float d=3.0f;
+			System.out.println(a/b);
+			System.out.println(c/d);
+			System.out.println(a/d);
+			
+			//단항 연산자
+			int i=3;
+			i++; //i = i+1
+			System.out.println(i);
+			System.out.println(i++);//i를 사용한 후 증가
+			System.out.println(i);
+			System.out.println(++i);//i를 증가한 후 사용
+			
+			//대입 연산자
+			int x=10;
+			System.out.println(x);
+			x=5;
+			x-=3;
+			System.out.println(x);
+			x=5;
+			x+=3;
+			System.out.println(x);
+			x=5;
+			x*=3;
+			System.out.println(x);
+			x=5;
+			x/=3;
+			System.out.println(x);
+			x=5;
+			x%=3;
+			System.out.println(x);*/
+			
+			/*//논리 연산자
+			int x =5;            //and
+			System.out.println(x>3 && x<10);
+			
+		                         //or
+			System.out.println(x>3 || x<4); // \쉬프트 눍
+			
+			                   //no     
+			System.out.println(!(x>3 && x<10));
+			
+			//조건 연산자(=삼항 연산자)
+			int num=10;
+			boolean isEven;
+			isEven = num%2==0?
+		    System.out.println(isEven);
+			
+			//비트 연산자
+			int x = 5;
+			int y = 10;
+			int z = -5;
+			System.out.println(x & y);
+			
+			System.out.println(x | y);
+			
+			System.out.println(x ^ y);
+			
+			System.out.println(~y);
+			
+			System.out.println(x<<2);
+			System.out.println(z<<2);
+			
+			System.out.println(x>>2);
+			System.out.println(y>>2);
+			System.out.println(z>>2);
+			
+			int num;
+			num = -5+3*10/2;
+			System.out.println(num);
+			
+			num =10;
+			System.out.println(num);
+			System.out.println(num++);
+			System.out.println(num);
+			System.out.println(--num);
+			
+			int num1=10;
+			int num2=20;
+			boolean result; 
+			result=((num1>10&&num2>10));
+			System.out.println(result);
+			result=((num1>10||num2>10));
+			System.out.println(result);
+			System.out.println(!result);
+			
+			int num3 =2;
+			int num4 =10;
+			System.out.println(num3&num4);
+			System.out.println(num3|num4);
+			System.out.println(num3^num4);
+			System.out.println(~num3);
+			
+			int num5 = 8;
+			System.out.println(num5 += 10);
+			System.out.println(num5 -= 10);
+			System.out.println(num5 >>= 2);
+			
+			int num6 =10;
+			int num7 =20;
+			int result1=(num6>=10) ? num7+10 : num7-10;
+			System.out.println(result1);*/
+			
+			//scanner 
+			System.out.println("문자열 입력:");
+			String inputString = axb.nextLine();
+			System.out.println(inputString);
+			System.out.println();
+			
+			
+			System.out.println("정수 입력:");
+			int inputint = axb.nextInt();
+			System.out.println(inputint);
+			System.out.println();
+			
+			System.out.println("실수 입력:");
+			double inputdouble = axb.nextDouble();
+			System.out.println(inputdouble);
+			System.out.println();
+			
+			System.out.println("숫자를 입력하세요:");
+			int num = axb.nextInt();
+			System.out.println("당신이 입력한 숫자는:"+num);
+			
+			System.out.println("이름을 입력해 주세요:");
+			String name = axb.nextLine();
+			System.out.println("당신의 이름:"+name);
+	}
+}
